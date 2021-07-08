0dfeead3cdcdf644491d6f1a5af3255ac2e30eb7
awangenh <aldo.vw@ufsc.br>
2021-07-08 15:16:34 -0300

    TPU #1.2

--- lib/models/bn_helper.py
+++ lib/models/bn_helper.py
@@ -1,13 +1,13 @@
-# This file has no references to torch.cuda or cudnn. Nothing changed.
+# SyncBatchNorm expects input tensor to be on a GPU and raises an error if it is a TPU
 
 import torch
 import functools
 
-if torch.__version__.startswith('0'):
-    from .sync_bn.inplace_abn.bn import InPlaceABNSync
-    BatchNorm2d = functools.partial(InPlaceABNSync, activation='none')
-    BatchNorm2d_class = InPlaceABNSync
-    relu_inplace = False
-else:
-    BatchNorm2d_class = BatchNorm2d = torch.nn.SyncBatchNorm
-    relu_inplace = True
+#if torch.__version__.startswith('0'):
+from .sync_bn.inplace_abn.bn import InPlaceABNSync
+BatchNorm2d = functools.partial(InPlaceABNSync, activation='none')
+BatchNorm2d_class = InPlaceABNSync
+relu_inplace = False
+#else:
+#    BatchNorm2d_class = BatchNorm2d = torch.nn.SyncBatchNorm
+#    relu_inplace = True
